cb08dce67823068f82e80059eafa30a28f7391c1
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2023-07-17 22:44:55 +0800

    Added Links and Default Error Page

--- src/components/ResponsiveNavBar.jsx
+++ src/components/ResponsiveNavBar.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react"
 import {
   Navbar,
   Collapse,
-  Typography,
   Button,
   IconButton,
   List,
@@ -16,58 +15,48 @@ import {
   FolderIcon,
   BookOpenIcon,
 } from "@heroicons/react/24/outline"
+import { Link } from "react-router-dom"
 
 function NavList() {
   return (
     <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="blue-gray"
-        className="font-serif font-medium text-md"
+      <a
+        href="#strategy"
+        className="text-blue-gray-900 font-serif font-medium text-md"
       >
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <GlobeAsiaAustraliaIcon className="h-[18px] w-[18px]" />
           Strategy
         </ListItem>
-      </Typography>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="blue-gray"
-        className="font-serif font-medium text-md"
+      </a>
+
+      <Link
+        to="/performance"
+        className="text-blue-gray-900 font-serif font-medium text-md"
       >
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <ChartBarSquareIcon className="h-[18px] w-[18px]" />
           Performance
         </ListItem>
-      </Typography>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="blue-gray"
-        className="font-serif font-medium text-md"
+      </Link>
+      <Link
+        to="/letters"
+        className="text-blue-gray-900 font-serif font-medium text-md"
       >
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <FolderIcon className="h-[18px] w-[18px]" />
-          Partnership Letters
+          Letters
         </ListItem>
-      </Typography>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="blue-gray"
-        className="font-serif font-medium text-md"
+      </Link>
+      <Link
+        to="/blog"
+        className="text-blue-gray-900 font-serif font-medium text-md"
       >
         <ListItem className="flex items-center gap-2 py-2 pr-4">
           <BookOpenIcon className="h-[18px] w-[18px]" />
           Blog
         </ListItem>
-      </Typography>
+      </Link>
     </List>
   )
 }
@@ -89,10 +78,10 @@ export default function ResponsiveNavBar() {
 
   return (
     <Navbar className="mx-auto max-w-screen-xl px-4 py-3 sticky top-0 rounded-none min-w-full">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <a href="#hero">
+      <div className="flex items-center justify-between text-blue-gray-900 scroll-smooth">
+        <Link href="/" className="scroll-smooth">
           <h1>Seraya Investment Partnership </h1>
-        </a>
+        </Link>
         <div className="hidden lg:block">
           <NavList />
         </div>
