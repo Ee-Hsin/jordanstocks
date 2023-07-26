3a2c5df83e23abc01f8182cafc19a268513147e8
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2023-07-26 13:52:44 +0800

    Added media querying for Letters Nav Bar

--- src/components/LettersNavBar.jsx
+++ src/components/LettersNavBar.jsx
@@ -1,20 +1,18 @@
-import { NavLink } from "react-router-dom"
-import "./LettersNavBar.css"
+// import { Link } from "react-router-dom"
+// import { useState } from "react"
+import { useMediaPredicate } from "react-media-hook"
 
 export const LettersNavBar = () => {
+  // Use this to turn the sidenav into a regular nav.
+  const mediumAndAbove = useMediaPredicate("(min-width: 768px)")
+
   return (
-    <div className="w-full md:w-[300px]">
-      <div
-        className="flex flex-row md:flex-col justify-center 
-      md:h-screen md:absolute md:top-0 md:z-0"
-      >
-        <NavLink to="/letters" className="m-2 p-2">
-          Annual/Interim Letters
-        </NavLink>
-        <NavLink to="positions" className="m-2 p-2">
-          Current Positions
-        </NavLink>
-      </div>
+    <div>
+      {!mediumAndAbove ? (
+        <div>Put a header Navbar here</div>
+      ) : (
+        <div>Put a Sidebar here</div>
+      )}
     </div>
   )
 }
