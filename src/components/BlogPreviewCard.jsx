0e017e1e4249850a2f12d2c8083ae6cbbb909be6
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2023-07-28 22:17:22 +0800

    Moved the Blog cards into their own component

---
+++ src/components/BlogPreviewCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+export const BlogPreviewCard = ({ id, img, title, date, description }) => {
+  return (
+    <li className="w-full mx-auto group sm:max-w-sm ">
+      <Link to={id}>
+        <img
+          src={img}
+          loading="lazy"
+          alt={title}
+          className="w-full md:w-[  320px] md:h-[200px] rounded-lg "
+        />
+        <div className="mt-3 space-y-2">
+          <span className="block text-indigo-600 text-sm">{date}</span>
+          <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-semibold">
+            {title}
+          </h3>
+          <p className="text-gray-600 text-sm duration-150 group-hover:text-gray-800">
+            {description.slice(0, 100) + "..."}
+          </p>
+        </div>
+      </Link>
+    </li>
+  )
+}
