29659f29467d3056cff88fdd4401458b3451bae4
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2023-07-30 02:18:27 +0800

    (Correct one) Added firestore getTImestamp functions.

--- src/hooks/firestore.js
+++ src/hooks/firestore.js
@@ -1,20 +1,66 @@
 import { db } from "../firebase"
-import { collection, getDocs, setDoc, doc } from "firebase/firestore"
+import {
+  collection,
+  getDocs,
+  setDoc,
+  doc,
+  serverTimestamp,
+} from "firebase/firestore"
 
+// TODO: Add an arguments for clauses and spread that as additional
+// parameters in getDocs.
 const getCollection = (collectionName) => {
   return getDocs(collection(db, collectionName))
 }
 
-const postDoc = (collectionName, docId, dataToStore) => {
-  // For if we provide a custom docId such as "emailList" in a collection called Newsletter
-  // kinda dumb but that's the way we're doing things)
+const postDoc = (collectionName, docId, docData) => {
+  // For if we provide a custom docId such as the email in a collection called "emailList"
+  // docData can then be both current date  of subscription (generate via firestore) and
+  // their email.
   if (docId) {
-    return setDoc(doc(db, collectionName, docId), dataToStore)
+    return setDoc(doc(db, collectionName, docId), docData)
     // If we did not provide a custom docId, we just reference a collection and when the doc is
     // added, it will be generated with a random Id.
   } else {
-    return setDoc(collection(collectionName), dataToStore)
+    return setDoc(collection(collectionName), docData)
   }
 }
 
-export { getCollection, postDoc }
+// Returns current firestore serverTimestamp (unlike regular firebase firestore Timestamp,
+// it cannot be read or converted to a date until it is fetched back from firestore)
+const getFirestoreTimestamp = () => {
+  return serverTimestamp()
+}
+
+// Converts to javascript timestamp
+const convertFirestoreTimestamp = (timestamp) => {
+  return timestamp.toDate()
+}
+
+export {
+  getCollection,
+  postDoc,
+  getFirestoreTimestamp,
+  convertFirestoreTimestamp,
+}
+
+// The below code works, but the getTimestamp uses Timestamp from firebase/firestore.
+// I rather use serverTimestamp(). Which SHOULD work it just doesn't work directly with m testing
+// because it needs to go into the server first before it can be converted into a Timestamp.
+/*The second function works fine regardless as long as it has a viable timestamp. I may use it
+  after all, to convert to nicer timsetamp formats once I am able to test how the 
+  convertFirestoreTimestamp function really works */
+
+// const getFirestoreTimestamp = () => {
+//   return Timestamp.now()
+// }
+
+// // Converts to javascript timestamp
+// const convertFirestoreTimestamp = (timestamp) => {
+//   let dateInMillis = timestamp * 1000
+//   let date = new Date(dateInMillis)
+//   let myDate = date.toLocaleDateString()
+//   let myTime = date.toLocaleTimeString()
+//   myDate = myDate.replaceAll("/", "-")
+//   return myDate + " " + myTime
+// }
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -35,7 +35,7 @@ export const BlogPage = () => {
                 />
               </svg>
               <input
-                type="text"
+                type="email"
                 placeholder="Enter your email"
                 className="w-full pl-12 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg sm:max-w-xs"
               />
