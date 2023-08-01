4598b2474766e88572b3358bb6835b5fd0bdd2d7
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2023-08-01 18:55:02 +0800

    moved logic from BlogPage to Blog Display

---
+++ src/components/Blog/BlogDisplay.jsx
@@ -0,0 +1,39 @@
+import { useGetBlogPosts } from "../../hooks/query"
+import { Loader } from "../UI/Loader"
+import { BlogPreviewCard } from "./BlogPreviewCard"
+import { SubscribeBlogComponent } from "./SubscribeBlogComponent"
+
+export const BlogDisplay = () => {
+  const { isLoading, isError, isSuccess, data, error } = useGetBlogPosts()
+
+  return (
+    <section className="pb-20 pt-8">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="space-y-5 sm:text-center sm:max-w-md sm:mx-auto">
+          <h1 className="text-gray-800 text-3xl font-extrabold sm:text-4xl">
+            Latest blog posts
+          </h1>
+          <p className="text-gray-600">
+            Keep up to date with our latest investment ideas
+          </p>
+          <SubscribeBlogComponent variant="blog" />
+        </div>
+        <ul className="grid gap-x-8 gap-y-10 mt-16 sm:grid-cols-2 lg:grid-cols-3">
+          {isError && <h1>Error: {JSON.stringify(error)}</h1>}
+          {isSuccess &&
+            data.docs.map((post) => (
+              <BlogPreviewCard
+                key={post.id}
+                id={post.id}
+                img={post.data().img}
+                title={post.data().title}
+                date={post.data().date}
+                description={post.data().description}
+              />
+            ))}
+        </ul>
+      </div>
+      {isLoading && <Loader />}
+    </section>
+  )
+}
